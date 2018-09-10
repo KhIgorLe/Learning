d4dd6a6c2673493e7bd84b0445538a0271d13d7e
Rybu system <KhIgorLe>
2018-09-10 23:01:22 +0300

    Task 2

---
+++ lesson_1/task2.rb
@@ -0,0 +1,7 @@
+puts "Введите основание треугольника:"
+base = gets.chomp.to_f
+
+puts "Введите высоту треугольника:"
+height = gets.chomp.to_f
+
+puts "Площадь треугольника равна #{base * height / 2} см2"
