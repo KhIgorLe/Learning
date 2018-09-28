4994879fb3da591f50be1695b8ea0fea90d29ecc
KhIgorLe <43117537+KhIgorLe@users.noreply.github.com>
2018-09-28 20:09:57 +0300

    task3 (#5)

---
+++ lesson_3/route.rb
@@ -0,0 +1,27 @@
+=begin
+Имеет начальную и конечную станцию, а также список промежуточных станций.
+Начальная и конечная станции указываютсся при создании маршрута, а промежуточные могут добавляться между ними.
+Может добавлять промежуточную станцию в список
+Может удалять промежуточную станцию из списка
+Может выводить список всех станций по-порядку от начальной до конечной
+=end
+
+class Route
+  attr_reader :stations
+
+  def initialize(begin_station, end_station)
+    @stations = [begin_station, end_station]
+  end
+
+  def add_intermediate_station(name_station)
+    @stations.insert(-2, name_station)
+  end
+
+  def del_intermediate_station(name_station)
+    @stations.delete(name_station) unless [@stations.first, @stations.last].include?(name_station)
+  end
+
+  def list_all_station
+    @stations.each { |name| puts "Станция #{name}" }
+  end
+end
---
+++ lesson_3/train.rb
@@ -0,0 +1,85 @@
+=begin
+Имеет номер (произвольная строка) и тип (грузовой, пассажирский) и количество вагонов, эти данные указываются
+при создании экземпляра класса
+Может набирать скорость
+Может возвращать текущую скорость
+Может тормозить (сбрасывать скорость до нуля)
+Может возвращать количество вагонов
+Может прицеплять/отцеплять вагоны (по одному вагону за операцию,
+метод просто увеличивает или уменьшает количество вагонов).
+Прицепка/отцепка вагонов может осуществляться только если поезд не движется.
+Может принимать маршрут следования (объект класса Route). 
+При назначении маршрута поезду, поезд автоматически помещается на первую станцию в маршруте.
+Может перемещаться между станциями, указанными в маршруте. Перемещение возможно вперед и назад,
+но только на 1 станцию за раз.
+Возвращать предыдущую станцию, текущую, следующую, на основе маршрута
+=end
+
+class Train
+  attr_reader :speed, :number_wagons, :type, :route, :number
+
+  def initialize(number, type, number_wagons, speed = 0)
+    @number = number
+    @type = type
+    @number_wagons = number_wagons
+    @speed = speed
+  end
+
+  def go(speed)
+    @speed += speed 
+  end
+
+  def decrease_speed(speed)
+    @speed -= speed if @speed > speed
+  end
+
+  def add_wagon
+    @number_wagons += 1 if @speed == 0
+  end
+
+  def del_wagon
+    @number_wagons -= 1 if @speed == 0 && @number_wagons.positive?
+  end
+
+  def current_station
+    @route.stations[@index_station]
+  end
+
+  def take_route(route)
+    @route = route
+    @index_station = 0
+    station_take_train
+  end
+
+  def station_take_train
+    current_station.take_train(self)
+  end
+
+  def station_send_train
+    current_station.send_train(self)
+  end
+
+  def go_next_station
+    if next_station
+      station_send_train
+      @index_station += 1
+      station_take_train
+    end
+  end
+
+  def go_previouse_station
+    if previouse_station
+      station_send_train
+      @index_station -= 1
+      station_take_train
+    end
+  end
+
+  def next_station
+    @route.stations[@index_station + 1]
+  end
+
+  def previouse_station
+    @route.stations[@index_station - 1] if @index_station > 0
+  end
+end
